a760def8cdc2bf4526c9c6701b472fa7d8ce1a0a
KoYoyoi <147980310+Koyoyoi@users.noreply.github.com>
2025-03-23 20:03:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ def handle_image(data):
         frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
         # 影像處理（轉成灰階）
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         _, buffer = cv2.imencode(".jpg", gray)  # 轉換為 JPG 格式
         processed_base64 = base64.b64encode(buffer).decode("utf-8")
 
