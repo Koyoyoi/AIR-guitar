857448089087acdd31885a2affad9b7a9ca4b0d3
KoYoyoi <147980310+Koyoyoi@users.noreply.github.com>
2025-03-23 20:33:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def handle_image(data):
         print("Error:", e)
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=10000. debug=True)
+    socketio.run(app, host="0.0.0.0", port=10000, debug=True)
